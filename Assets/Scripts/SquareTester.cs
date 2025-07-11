7c2c35b06c2ea3e3f6bd2bd7f08b519981d7fe29
Dora Choi <hellodoradora7@gmail.com>
2025-07-11 14:40:44 +0900

    feat: generate diamond mesh

--- Assets/Scripts/SquareTester.cs
+++ Assets/Scripts/SquareTester.cs
@@ -9,7 +9,13 @@ public class SquareTester : MonoBehaviour
     Vector2 bottomLeft;
     Vector2 topLeft;
 
-    [Header( " Elements " )]
+    Vector2 topCenter;
+    Vector2 rightCenter;
+    Vector2 bottomCenter;
+    Vector2 leftCenter;
+    
+
+    [Header(" Elements ")]
     [SerializeField] private MeshFilter filter;
 
     [Header(" Settings ")]
@@ -23,13 +29,19 @@ public class SquareTester : MonoBehaviour
         bottomLeft = bottomRight + Vector2.left * gridScale;
         topLeft = bottomLeft + Vector2.up * gridScale;
 
+        topCenter = topRight + Vector2.left * gridScale / 2;
+        rightCenter = bottomRight + Vector2.up * gridScale / 2;
+        bottomCenter = bottomLeft + Vector2.right * gridScale / 2;
+        leftCenter = topLeft + Vector2.down * gridScale / 2;
+
+
         Vector3[] vertices = new Vector3[4];
         int[] triangles = new int[6];
 
-        vertices[0] = topRight;
-        vertices[1] = bottomRight;
-        vertices[2] = bottomLeft;
-        vertices[3] = topLeft;
+        vertices[0] = topCenter;
+        vertices[1] = rightCenter;
+        vertices[2] = bottomCenter;
+        vertices[3] = leftCenter;
 
         triangles[0] = 0;
         triangles[1] = 1;
@@ -56,9 +68,15 @@ public class SquareTester : MonoBehaviour
     {
         Gizmos.color = Color.red;
 
-        Gizmos.DrawSphere(topRight, gridScale /4f);
-        Gizmos.DrawSphere(bottomLeft, gridScale /4f);
-        Gizmos.DrawSphere(bottomRight, gridScale /4f);
-        Gizmos.DrawSphere(topLeft, gridScale /4f);
+        Gizmos.DrawSphere(topRight, gridScale / 4f);
+        Gizmos.DrawSphere(bottomLeft, gridScale / 4f);
+        Gizmos.DrawSphere(bottomRight, gridScale / 4f);
+        Gizmos.DrawSphere(topLeft, gridScale / 4f);
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawSphere(topCenter, gridScale / 8f);
+        Gizmos.DrawSphere(rightCenter, gridScale / 8f);
+        Gizmos.DrawSphere(bottomCenter, gridScale / 8f);
+        Gizmos.DrawSphere(leftCenter, gridScale / 8f);
     }
 }
