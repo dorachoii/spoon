e34a711b01fb962a292427112c7e392966b9bc04
Dora Choi <hellodoradora7@gmail.com>
2025-07-24 21:43:30 +0900

    assets : add joystick

---
+++ Assets/Joystick Pack/Examples/JoystickPlayerExample.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoystickPlayerExample : MonoBehaviour
+{
+    public float speed;
+    public FloatingJoystick floatingJoystick;
+    public Rigidbody2D rb;
+
+    public void FixedUpdate()
+    {
+        Vector3 direction = Vector3.up * floatingJoystick.Vertical + Vector3.right * floatingJoystick.Horizontal;
+        rb.AddForce(direction * speed * Time.fixedDeltaTime, ForceMode2D.Force);
+    }
+}
