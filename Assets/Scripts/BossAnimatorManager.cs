c8757b5cea9eabbd5b36614530e516cd9bb1ae46
Dora Choi <hellodoradora7@gmail.com>
2025-07-31 15:02:41 +0900

    assets: add sprites(enemy, item etc.)

---
+++ Assets/Scripts/BossAnimatorManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossAnimatorManager : MonoBehaviour
+{
+    public Animator[] bodies;
+    private float interval = 0.5f;
+    public string animationName = "Blue Idle - Animation";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(PlayStaggered());
+    }
+
+    private IEnumerator PlayStaggered()
+    {
+        for (int i = 0; i < bodies.Length; i++)
+        {
+            var animator = bodies[i];
+            animator.Play(animationName, 0, 0f); // 0초부터 재생
+            animator.speed = 1f;
+            yield return new WaitForSeconds(interval);
+        }
+    }
+}
